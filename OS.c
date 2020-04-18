6569cf1196d4be848e8036cac98de0930e623781
saiyamvikram <63245864+saiyamvikram@users.noreply.github.com>
2020-04-18 20:41:02 +0530

    Update OS.c

--- OS.c
+++ OS.c
@@ -33,3 +33,25 @@ scanf("%d", &p_queue[i].at ); printf("Enter Burst Time:\t");
 scanf("%d", &p_queue[i].bt); printf("Enter Priority:\t"); scanf("%d", &p_queue[i].priority); p_queue[i].status = 0;
 bt = bt + p_queue[i].bt;
 }
+at_Sorting(); p_queue[9].priority = -9999;
+printf("\nProcess Name\tArrival Time\tBurst Time\tPriority\tWaiting Time"); for(time = p_queue[0].at; time < bt;)
+{
+largest = 9;
+for(i = 0; i < limit; i++)
+{
+if(p_queue[i].at <= time && p_queue[i].status != 1 && p_queue[i].priority
+> p_queue[largest].priority)
+{
+largest = i;
+}
+}
+time = time + p_queue[largest].bt; p_queue[largest].ct = time;
+p_queue[largest].wt = p_queue[largest].ct - p_queue[largest].at - p_queue[largest].bt;
+p_queue[largest].tat = p_queue[largest].ct - p_queue[largest].at; p_queue[largest].status = 1;
+wait_time = wait_time + p_queue[largest].wt; tat = tat + p_queue[largest].tat;
+printf("\n%c\t\t%d\t\t%d\t\t%d\t\t%d", p_queue[largest].p_name, p_queue[largest].at, p_queue[largest].bt, p_queue[largest].priority, p_queue[largest].wt);
+}
+Avg_wt = wait_time / limit; Avg_tat = tat / limit;
+printf("\n\nAvg waiting time:\t%f\n", Avg_wt); printf("Avg Turnaround Time:\t%f\n", Avg_tat);
+
+}
